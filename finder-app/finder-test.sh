09b1ad4328a8b36f765b3b343d7ce359ef3d8858
undone97 <brunomsssantos@gmail.com>
2023-06-13 14:25:16 +0100

    fix file created as dir

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -36,7 +36,7 @@ assignment=`cat /etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
-	mkdir -p "$WRITEDIR"
+	#mkdir -p "$WRITEDIR"
 
 	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
 	#The quotes signify that the entire string in WRITEDIR is a single string.
@@ -54,7 +54,7 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	writer.sh "$WRITEDIR" "$WRITESTR"
+	writer "$WRITEDIR" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
